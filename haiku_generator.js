086d9cd484472e842c35abe81371ebf9b5f34cba
Aditya K <adityavkk@gmail.com>
2016-07-20 20:58:39 -0400

    pattern change to reflect traditional Haiku structure

--- haiku_generator.js
+++ haiku_generator.js
@@ -3,12 +3,13 @@ var headlinesHaiku = require('./headlinesApi/headlinesApi');
 
 var dictPath = './cmudict.txt';
 var pattern = [
-  [5, 2, 5],
-  [3, 2, 1],
-  [2, 3, 5]
+  [1, 2, 2],
+  [2, 2, 3],
+  [1, 2, 2]
 ];
 var textsPaths = ['./texts/trial.txt', './texts/metamorphosis.txt'];
 
 console.log('***** Kafka Haiku *****', '\n' + haiku.createHaiku(pattern, dictPath, textsPaths) + '\n');
 console.log('***** Headlines Haiku *****');
 headlinesHaiku(haiku.createHaiku, [pattern, dictPath, []], console.log);
+
